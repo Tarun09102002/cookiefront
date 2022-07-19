4614d12eaeb4635606984f41f5b6a67b296e885a
Tarun <tarun.agrawal@spit.ac.in>
2022-07-19 09:47:14 +0530

    safety commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useRef, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const input = useRef();
+  const [name, setName] = useState();
+
+  const storeCookie = async () => {
+    try {
+      const name = input.current.value;
+      const { data } = await axios.post("/new", { name }, { withCredentials: true });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getCookie = async () => {
+    try {
+      const { data } = await axios.get("/name", { withCredentials: true });
+      console.log(data)
+      setName(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <p>{name}</p>
+        <label htmlFor="input">UserName</label>
+        <input ref={input} className="id" />
+        <button onClick={storeCookie}>Store Cookie</button>
+        <button onClick={getCookie}>Retrive Cookie</button>
+      </div>
     </div>
   );
 }
